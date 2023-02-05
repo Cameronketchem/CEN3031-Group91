bf8cfcda647498c1e4742b291a87a81cc20dbb35
zeim839 <zeim839@users.noreply.github.com>
2023-02-05 17:57:49 -0500

    lint: formatted code

--- server/data/queries_test.go
+++ server/data/queries_test.go
@@ -23,124 +23,124 @@ func TestQueryAssetByID(t *testing.T) {
 	str := initTestStore(t)
 	defer str.Close()
 
-  for i := 0; i<100; i++ {
-    _, err := str.QueryAssetById(i)
-    require.NoError(t, err)
-  }
+	for i := 0; i < 100; i++ {
+		_, err := str.QueryAssetById(i)
+		require.NoError(t, err)
+	}
 }
 
 func TestQueryContractByID(t *testing.T) {
 	str := initTestStore(t)
 	defer str.Close()
 
-  for i := 0; i<100; i++ {
-    _, err := str.QueryContractById(i)
-    require.NoError(t, err)
-  }
+	for i := 0; i < 100; i++ {
+		_, err := str.QueryContractById(i)
+		require.NoError(t, err)
+	}
 }
 
 func TestQueryUserByID(t *testing.T) {
 	str := initTestStore(t)
 	defer str.Close()
 
-  for i := 0; i<7; i++ {
-    _, err := str.QueryUserById(i)
-    require.NoError(t, err)
-  }
+	for i := 0; i < 7; i++ {
+		_, err := str.QueryUserById(i)
+		require.NoError(t, err)
+	}
 }
 
 func TestQueryContribByID(t *testing.T) {
 	str := initTestStore(t)
 	defer str.Close()
 
-  for i := 0; i<100; i++ {
-    _, err := str.QueryContribById(i)
-    require.NoError(t, err)
-  }
+	for i := 0; i < 100; i++ {
+		_, err := str.QueryContribById(i)
+		require.NoError(t, err)
+	}
 }
 
 func TestInsertAsset(t *testing.T) {
-  str := initTestStore(t)
+	str := initTestStore(t)
 	defer str.Close()
 
-  for i := 0; i<100; i++ {
-    asset := Asset {i, i, i, "website.com", i, "lorem ipsum", 253.44}
-    err := str.InsertAsset(asset)
-    require.NoError(t, err)
-  }
+	for i := 0; i < 100; i++ {
+		asset := Asset{i, i, i, "website.com", i, "lorem ipsum", 253.44}
+		err := str.InsertAsset(asset)
+		require.NoError(t, err)
+	}
 }
 
 func TestInsertContract(t *testing.T) {
-  str := initTestStore(t)
+	str := initTestStore(t)
 	defer str.Close()
 
-  for i := 0; i<100; i++ {
-    contract := Contract {i, "0xFFFFFFFFFF", false, false}
-    err := str.InsertContract(contract)
-    require.NoError(t, err)
-  }
+	for i := 0; i < 100; i++ {
+		contract := Contract{i, "0xFFFFFFFFFF", false, false}
+		err := str.InsertContract(contract)
+		require.NoError(t, err)
+	}
 }
 
 func TestInsertUser(t *testing.T) {
-  str := initTestStore(t)
+	str := initTestStore(t)
 	defer str.Close()
 
-  for i := 0; i<100; i++ {
-    user := User {i, "0xFFAA99999", "website.com", "lorem ipsum"}
-    err := str.InsertUser(user)
-    require.NoError(t, err)
-  }
+	for i := 0; i < 100; i++ {
+		user := User{i, "0xFFAA99999", "website.com", "lorem ipsum"}
+		err := str.InsertUser(user)
+		require.NoError(t, err)
+	}
 }
 
 func TestInsertContrib(t *testing.T) {
-  str := initTestStore(t)
+	str := initTestStore(t)
 	defer str.Close()
 
-  for i := 0; i<100; i++ {
-    contrib := Contribution {i, i, i, 253.4, ContribPending}
-    err := str.InsertContrib(contrib)
-    require.NoError(t, err)
-  }
+	for i := 0; i < 100; i++ {
+		contrib := Contribution{i, i, i, 253.4, ContribPending}
+		err := str.InsertContrib(contrib)
+		require.NoError(t, err)
+	}
 }
 
 func TestGetUsers(t *testing.T) {
-  str := initTestStore(t)
+	str := initTestStore(t)
 	defer str.Close()
 
-  // Get first 20 users (seed database only has 7)
-  users, err := str.GetUsers(-1)
-  require.NoError(t, err)
-  require.LessOrEqual(t, len(users), 20)
-  require.Equal(t, users[0].UserID, 0)
+	// Get first 20 users (seed database only has 7)
+	users, err := str.GetUsers(-1)
+	require.NoError(t, err)
+	require.LessOrEqual(t, len(users), 20)
+	require.Equal(t, users[0].UserID, 0)
 
-  // Expect no users
-  usersB, err := str.GetUsers(20)
-  require.NoError(t, err)
-  require.Equal(t, len(usersB), 0)
+	// Expect no users
+	usersB, err := str.GetUsers(20)
+	require.NoError(t, err)
+	require.Equal(t, len(usersB), 0)
 }
 
 func TestGetAssets(t *testing.T) {
-  str := initTestStore(t)
+	str := initTestStore(t)
 	defer str.Close()
 
-  // Get first 20 assets
-  assets, err := str.GetAssets(-1)
-  require.NoError(t, err)
-  require.LessOrEqual(t, len(assets), 20)
-  require.Equal(t, assets[0].AssetID, 0)
+	// Get first 20 assets
+	assets, err := str.GetAssets(-1)
+	require.NoError(t, err)
+	require.LessOrEqual(t, len(assets), 20)
+	require.Equal(t, assets[0].AssetID, 0)
 
-  // Get 5 more sets of 20
-  for i := 1; i<5; i++ {
-    index := i*20
-    assets, err = str.GetAssets(index)
-    require.NoError(t, err)
-    require.LessOrEqual(t, len(assets), 20)
-    require.Equal(t, assets[0].AssetID, index+1)
-  }
+	// Get 5 more sets of 20
+	for i := 1; i < 5; i++ {
+		index := i * 20
+		assets, err = str.GetAssets(index)
+		require.NoError(t, err)
+		require.LessOrEqual(t, len(assets), 20)
+		require.Equal(t, assets[0].AssetID, index+1)
+	}
 
-  // Should return nothing
-  assets, err = str.GetAssets(101)
-  require.NoError(t, err)
-  require.LessOrEqual(t, len(assets), 20)
-  require.Equal(t, len(assets), 0)
+	// Should return nothing
+	assets, err = str.GetAssets(101)
+	require.NoError(t, err)
+	require.LessOrEqual(t, len(assets), 20)
+	require.Equal(t, len(assets), 0)
 }
