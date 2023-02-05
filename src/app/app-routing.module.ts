c1d4056e789bae5b96cfc2e8da57b47c76bc1052
Cameronketchem <cameronketchem@gmail.com>
2023-02-05 17:08:57 -0500

    feat: Using the app component template created welcome landing page that will eventually link to both a login and signup page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
+import { SignupComponent } from './signup/signup.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [  { path: '/signup/signup', component: SignupComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
