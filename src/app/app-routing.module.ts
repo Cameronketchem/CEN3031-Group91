f7025b4aca03930e4d629add817717073f60f5fc
Cameronketchem <cameronketchem@gmail.com>
2023-02-06 13:35:55 -0500

    feat: Finalized routing between the home, login and signup pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
+import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [  { path: '/signup/signup', component: SignupComponent },];
+const routes: Routes = [  {path:'', component: HomeComponent},
+{ path: 'signup', component: SignupComponent },
+{path: 'login', component: LoginComponent},];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), RouterModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +11,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'CrowdNFT';
 }
+
