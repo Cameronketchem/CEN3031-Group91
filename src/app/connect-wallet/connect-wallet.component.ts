cb97662dcf50523137aad622f62cbce472da3009
Cameronketchem <cameronketchem@gmail.com>
2023-02-21 12:33:12 -0500

    feat:Added functionality to wallet connection button

--- src/app/connect-wallet/connect-wallet.component.ts
+++ src/app/connect-wallet/connect-wallet.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import Web3 from 'web3';
+import { AbiItem } from 'web3-utils';
+
+declare let window: any;
+let web3: Web3;
 
 @Component({
   selector: 'app-connect-wallet',
   templateUrl: './connect-wallet.component.html',
   styleUrls: ['./connect-wallet.component.css']
 })
-export class ConnectWalletComponent {
 
+
+export class ConnectWalletComponent {
+  web3: any
+
+  public async login() {
+    try {
+      await window.ethereum.enable();
+      this.web3 = new Web3(window.ethereum);
+      console.log('Web3 instance:', this.web3);
+      // Perform other actions with the web3 instance as needed
+    } catch (error) {
+      console.error(error);
+    }
+  }
+    
+  
+  async readAccount() {
+    const accounts = await this.web3.eth.getAccounts();
+    console.log('Accounts:', accounts);
+  }
 }
