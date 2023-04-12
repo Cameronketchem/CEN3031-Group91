80144a554540c0adc4030a5d1acd4609b35e74fb
Sebastian <sebi_sarmiento@hotmail.com>
2023-04-12 13:57:16 -0400

    Added profile view component to routing service

--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './profile-view.component.html',
   styleUrls: ['./profile-view.component.css']
 })
+
 export class ProfileViewComponent {
+
+  userData: {
+    user_id: string,
+    wallet_pubkey: string,
+    public_pic_url: string,
+    bio: string
+  };
+  
+  constructor() {
+    // Will set values once we implement call to API
+    this.userData = {
+      user_id: 'Username',
+      wallet_pubkey: '',
+      public_pic_url: '',
+      bio: ''
+    };
+  }
 
 }
