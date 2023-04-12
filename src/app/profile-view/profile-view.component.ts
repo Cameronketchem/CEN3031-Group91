d2f9b8482327c8f4941670d9200f66c92269b95c
Sebastian <sebi_sarmiento@hotmail.com>
2023-04-12 13:37:49 -0400

    Adding a profile view component that will show account information

---
+++ src/app/profile-view/profile-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-view',
+  templateUrl: './profile-view.component.html',
+  styleUrls: ['./profile-view.component.css']
+})
+export class ProfileViewComponent {
+
+}
