2b0d1ee24d98bd4df13830b830bb95aaa65dd8f3
zeim839 <zeim839@users.noreply.github.com>
2023-04-17 19:07:29 -0400

    feat: added .env environment

--- server/main.go
+++ server/main.go
@@ -1,8 +1,38 @@
 package main
 
-import "github.com/Cameronketchem/CEN3031-Group91/server/routes"
+import (
+  "fmt"
+  "log"
+  "os"
+  "github.com/joho/godotenv"
+  "github.com/Cameronketchem/CEN3031-Group91/server/routes"
+)
+
+// Returns an environment variable, issues error
+// if it doesnt exist.
+func getVar(key string) string {
+  value, exists := os.LookupEnv(key)
+  if !exists {
+    log.Fatal(fmt.Sprintf("Expected %s environment variable to be set", key))
+  }
+
+  return value
+}
 
 func main() {
-	api := routes.New("server.sqlite3", "0x123456789ABCDEF", false)
+  // Load environment variables.
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+  // Retrieve environment variables.
+  dbpath := getVar("DB_PATH")
+  secret := getVar("SECRET")
+  privKey := getVar("PRIV_KEY")
+  nodeAddr := getVar("NODE_ADDR")
+
+  // Serve on localhost:8080
+  api := routes.New(dbpath, secret, privKey, nodeAddr, false)
 	api.Start(":8080", true)
 }
