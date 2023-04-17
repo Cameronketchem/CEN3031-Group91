7779474424ff16c568844b101e00761712233a49
Cameronketchem <cameronketchem@gmail.com>
2023-04-16 22:29:26 -0400

    feat: adding home component unit tests

---
+++ src/app/account.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AccountService } from './account.service';
+
+describe('AccountService', () => {
+  let service: AccountService;
+
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AccountService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
